375bcca767a442c9c853a0e72b2e3db4312c6887
emartinez5 <emartinez5@hawk.iit.edu>
2023-11-06 22:48:19 -0600

    Update script.js
    added alert text

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ function init(){
 	var button = document.getElementById('entrybutton');
 	function showMeText(){
 		var textbox = document.getElementById('entryinput');
+		document.getElementById('textoutput').innerHTML = textbox.value;
+		alert("Enrique Martinez:" + textbox.value);
 		
 	
 	
